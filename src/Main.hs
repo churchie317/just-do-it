65e4c5515747c9a28bd37510291a3f058487e01c
Eric <erchurchill@firstrepublic.com>
2018-04-27 15:48:36 -0700

    initial commit ª

---
+++ src/Main.hs
@@ -0,0 +1,116 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main where
+
+import GHC.Generics
+import Control.Monad (when)
+import Data.Aeson
+import Data.Maybe (fromMaybe, catMaybes)
+import Data.List (sortOn, groupBy, intercalate)
+import Data.Monoid
+import Data.Time.Clock
+import Data.Time.Format
+import System.Directory (doesFileExist, getHomeDirectory)
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as B
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+
+-- Task.hs
+
+data TaskGroup = TaskGroup {
+  groupTime :: UTCTime,
+  tasks :: [Task]
+} deriving (Show)
+
+data Task = Task {
+  text :: String,
+  time :: UTCTime
+} deriving (Show, Generic)
+
+instance ToJSON Task
+instance FromJSON Task
+
+databaseFilePath :: IO FilePath
+databaseFilePath = do
+  home <- getHomeDirectory
+  return $ home ++ "/.doing_data"
+
+createDatabase :: IO ()
+createDatabase = do
+  path <- databaseFilePath
+  writeFile path ""
+
+databaseExists :: IO Bool
+databaseExists =
+  databaseFilePath >>= doesFileExist
+
+confirmDatabase :: IO ()
+confirmDatabase = do
+  exists <- databaseExists
+  when (not exists) createDatabase
+
+-- Reads the tasks from disk
+readTasks :: IO [Task]
+readTasks = do
+  confirmDatabase
+  path <- databaseFilePath
+  maybeTasks <- fmap decodeStrict $ B.readFile path
+  return $ fromMaybe [] maybeTasks
+
+-- Writes the tasks to disk
+saveTasks :: [Task] -> IO ()
+saveTasks tasks = do
+  path <- databaseFilePath
+  L.writeFile path $ encode tasks
+
+mkTask :: String -> IO Task 
+mkTask text = do
+  utcTime <- getCurrentTime
+  return $ Task text utcTime
+
+-- Formats a list of tasks into a string
+showTasks :: [Task] -> String
+showTasks tasks =
+  undefined
+
+mkTaskGroup :: [Task] -> Maybe TaskGroup
+mkTaskGroup tasks@((Task _ time):_) = Just $ TaskGroup time tasks
+mkTaskGroup _ = Nothing
+
+
+groupTasks :: [Task] -> [TaskGroup]
+groupTasks tasks =
+  catMaybes $ map mkTaskGroup groupedTasks 
+  where
+     groupTasks (Task _ t1) (Task _ t2) = utctDay t1 == utctDay t2
+     groupedTasks = groupBy groupTasks $ sortOn time tasks
+
+renderTaskGroups :: [TaskGroup] -> String
+renderTaskGroups = intercalate "\n\n" . map renderTaskGroup
+
+renderTaskGroup :: TaskGroup -> String
+renderTaskGroup (TaskGroup time tasks) =
+  intercalate "\n" $ formatTime defaultTimeLocale "%A, %B %e" time
+  : map renderTask tasks
+
+renderTask :: Task -> String
+renderTask (Task text _) = "- " <> text
+
+createTask :: String -> IO ()
+createTask text = do
+  tasks <- readTasks
+  task <- mkTask text
+  saveTasks $ task : tasks
+
+-- Tuesday, January 28th
+-- - Wrote a haskell app
+-- - Refactored Accountify again
+-- 
+-- Wednesday, January 29th
+-- - Used the bathroom 5 times
+-- - Refactored Accountify again
